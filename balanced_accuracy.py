eea89a0dc6d619b7000d281e1713affbc7458d99
mohit-baliyan <mohitbaliyan98@gmail.com>
2021-11-23 22:44:01 +0000

    updated Nov 23

--- balanced_accuracy.py
+++ balanced_accuracy.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import numpy as np
 import pandas as pd
 from sklearn.metrics import balanced_accuracy_score
@@ -9,16 +11,28 @@ def save_folds(X, y):
     # create dataframe df and later join vectors in X and vector y
     df = pd.DataFrame(X)
     df['y'] = y
+
+    # create directory for saving folds
+    os.mkdir('folds')
+
     # initialize Stratified K fold cross validation
     skf = StratifiedKFold(n_splits=10, shuffle=True)
+
+    # looping over folds
     fold_no = 1
     for train_index, val_index in skf.split(df, y):
+        # slicing
         train = df.loc[train_index, :]
         val = df.loc[val_index, :]
+
+        # reset index for each fold data
         train.reset_index(inplace=True)
         val.reset_index(inplace=True)
+
+        # save to HDD
         train.to_csv('folds/train_fold_' + str(fold_no) + '.csv', index=False)
         val.to_csv('folds/val_fold_' + str(fold_no) + '.csv', index=False)
+
         fold_no += 1
 
 
@@ -26,10 +40,12 @@ def save_folds(X, y):
 def calculate_accuracy(X, y, model):
     # create folds and write to HDD
     save_folds(X, y)
+
     # outer loop to calculate the balanced accuracy 10 times
     balanced_accuracies = []
     for i in range(0, 10):
         balanced_accuracy_10_folds = []
+
         # inner loop for 10 fold stratified cross validation
         for j in range(1, 11):
             # read folds from csv and convert into numpy array
@@ -37,13 +53,20 @@ def calculate_accuracy(X, y, model):
             df_test = pd.read_csv('folds/val_fold_' + str(j) + '.csv', index_col=0)
             df_train = df_train.values
             df_test = df_test.values
+
             # slicing into X_train, y_train, X_test and y_test
             X_train = df_train[:, :-1]
             y_train = df_train[:, -1]
             X_test = df_test[:, :-1]
             y_test = df_test[:, -1]
+
             # train model and predict X_test
             model.fit(X_train, y_train)
             balanced_accuracy_10_folds.append(balanced_accuracy_score(y_test, model.predict(X_test)))
+
         balanced_accuracies.append(np.mean(balanced_accuracy_10_folds))
+
+    # delete directory
+    shutil.rmtree('folds')
+
     return np.mean(balanced_accuracies)
--- knn_results.py
+++ knn_results.py
@@ -1,4 +1,3 @@
-import os
 from pca import PCA
 from knn import KNN
 import pandas as pd
@@ -8,40 +7,38 @@ from balanced_accuracy import calculate_accuracy
 
 
 def main():
-    datasets = []
-    accuracy_original = []
-    accuracy_kaiser = []
-    accuracy_broken = []
-    accuracy_cn = []
+    # read dimensions
     df = pd.read_csv('frames.csv')
-    databases = os.listdir('./Databases/')
-    for database in databases:
-        # load dataset from Databases
-        data = loadmat('./Databases/' + database)
-        X = data['X']
-        y = data['Y']
-        # standardise data
-        X = stats.zscore(X)
-        # initialize KNN model
-        knn = KNN(3)
-        accuracy_original.append(calculate_accuracy(X, y, knn))
-        pca = PCA()
-        # for kaiser rule
-        accuracy_kaiser.append(
-            calculate_accuracy(pca.transformation(X, df[df['Databases'] == database]['PCA-K'].item()), y, knn))
-        # for broken stick
-        accuracy_broken.append(
-            calculate_accuracy(pca.transformation(X, df[df['Databases'] == database]['PCA-BS'].item()), y, knn))
-        # for conditional number
-        accuracy_cn.append(calculate_accuracy(pca.transformation(X, df[df['Databases'] == database]['PCA-CN'].item()),
-                                              y, knn))
-        datasets.append(database)
-    dictionary = {'Databases': datasets, 'Accuracy-O': accuracy_original, 'Accuracy-K': accuracy_kaiser,
-                  'Accuracy-BS': accuracy_broken, 'Accuracy-CN': accuracy_cn}
 
-    # convert dictionary into pandas dataframe and save into csv
-    acc = pd.DataFrame(dictionary)
-    acc.to_csv('performance_knn.csv')
+    # load database
+    database = 'spect.mat'
+    data = loadmat('./Databases/' + database)
+    X = data['X']
+    y = data['Y']
+
+    # standardise data
+    X = stats.zscore(X)
+
+    # initialize KNN model
+    knn = KNN(3)
+
+    # without transformation
+    accuracy_original = calculate_accuracy(X, y, knn)
+
+    # initialize pca
+    pca = PCA()
+
+    # after kaiser rule
+    accuracy_kaiser = calculate_accuracy(pca.transformation(X, df[df['Databases'] == database]['PCA-K'].item()), y, knn)
+
+    # after broken stick
+    accuracy_bs = calculate_accuracy(pca.transformation(X, df[df['Databases'] == database]['PCA-BS'].item()), y, knn)
+
+    # after conditional number
+    accuracy_cn = calculate_accuracy(pca.transformation(X, df[df['Databases'] == database]['PCA-CN'].item()), y, knn)
+
+    print(database, round(accuracy_original, 2), round(accuracy_kaiser, 2), round(accuracy_bs, 2),
+          round(accuracy_cn, 2))
 
 
 if __name__ == "__main__":
